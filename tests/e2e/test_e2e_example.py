401a16651cc5f49040f4317f506a3db135202656
chirayu upadhyay <60225354+uchirayu@users.noreply.github.com>
2025-12-11 15:03:37 +0100

    Change test URL to GitHub API

--- tests/e2e/test_e2e_example.py
+++ tests/e2e/test_e2e_example.py
@@ -1,5 +1,5 @@
 import requests
 
 def test_ping_service():
-    resp = requests.get("https://httpbin.org/get")
+    resp = requests.get("https://api.github.com")
     assert resp.status_code == 200
